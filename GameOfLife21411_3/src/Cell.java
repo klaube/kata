8fe897ddcec4942b37cbbe612069a9565258d6ef
Katharina Laube <blueflower@gmx.de>
2014-11-15 14:20:49 +0100

    Testcase #3 fails

--- GameOfLife21411_3/src/Cell.java
+++ GameOfLife21411_3/src/Cell.java
@@ -1,8 +1,24 @@
 
 public class Cell {
 
+	boolean alive = true;
+	
 	public boolean isAlive() {
-		return true;
+		return alive;
+	}
+
+	public void tick() {
+		alive = false;
+	}
+
+	public void setAlive(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addNeighbour(Cell n1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- GameOfLife21411_3/test/CellTest.java
+++ GameOfLife21411_3/test/CellTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,6 +9,31 @@ public class CellTest {
 	@Test
 	public void a_new_cell_should_be_alive() throws Exception {
 		Cell cut = new Cell();
+		assertTrue(cut.isAlive());
+	}
+	
+	@Test
+	public void a_lonely_cell_should_die() throws Exception {
+		Cell cut = new Cell();
+		cut.tick();
+		assertFalse(cut.isAlive());
+	}
+	
+	@Test
+	public void a_new_star_is_born() throws Exception {
+		Cell cut = new Cell();
+		cut.setAlive(false);
+		
+		Cell n1 = new Cell();
+		Cell n2 = new Cell();
+		Cell n3 = new Cell();		
+		
+		cut.addNeighbour(n1);
+		cut.addNeighbour(n2);
+		cut.addNeighbour(n3);
+		
+		cut.tick();
+		
 		assertTrue(cut.isAlive());
 	}
 }
